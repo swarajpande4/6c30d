3f495a16174cc95d6efbeb2822854250cdc2a463
swarajpande4 <swarajpande4@gmail.com>
2022-01-01 11:51:41 +0530

    Added Overlapping Rectangles

---
+++ d0-4/02-Overlapping-Rectangles.cpp
@@ -0,0 +1,18 @@
+// Overlapping Rectangles: https://practice.geeksforgeeks.org/problems/overlapping-rectangles1924/1
+
+class Solution
+{
+public:
+    int doOverlap(int L1[], int R1[], int L2[], int R2[])
+    {
+        // If one rectangle is left of other
+        if (R1[0] < L2[0] || R2[0] < L1[0])
+            return false;
+
+        // If one rectangle is bottom of the other
+        if (L1[1] < R2[1] || L2[1] < R1[1])
+            return false;
+
+        return true;
+    }
+};
