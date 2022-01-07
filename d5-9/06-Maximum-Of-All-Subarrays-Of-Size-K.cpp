1f120ea9e94d10331b720b5af8083d9526502c36
swarajpande4 <swarajpande4@gmail.com>
2022-01-07 16:28:25 +0530

    Added Maximum of all subarrays of size K

---
+++ d5-9/06-Maximum-Of-All-Subarrays-Of-Size-K.cpp
@@ -0,0 +1,52 @@
+// Maximum of all subarrays of size K: https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k3101/1
+
+class Solution
+{
+public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k)
+    {
+        vector<int> res;
+        deque<int> q;
+
+        int i = 0;
+
+        // Iterating over first k elements (1st window)
+        for(i = 0; i < k; i++)
+        {
+            // For each element, the previously smaller elements 
+            // are of no use, so we simply remove them from deque
+            while(!q.empty() && arr[i] >= arr[q.back()])
+                q.pop_back();
+            
+            // Adding new element at the back of the deque
+            q.push_back(i);
+        }
+
+        // Iterating over the rest of the elements
+        for(; i < n; i++)
+        {
+            // The element at the front of deque is the largest
+            // of the previous window, adding it to result.
+            res.push_back(arr[q.front()]);
+
+            // Removing elements which are out of this window
+            while(!q.empty() && q.front() <= i - k)
+                q.pop_front();
+            
+            // Removing all the elements which are smaller than 
+            // the element added currently.
+            while(!q.empty() && (arr[i] >= arr[q.back()])) 
+            q.pop_back();
+
+            // Adding new element at the back of the deque
+            q.push_back(i);
+        }
+
+        // Finally the element at the front fo the deque is the largest
+        // element of last window, so we simply add it to the list.
+        res.push_back(arr[q.front()]);
+        q.pop_front();
+
+        return res;
+    }
+};
