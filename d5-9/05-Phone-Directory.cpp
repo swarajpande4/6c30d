46d001e6cd50d2c1d54d330ec56e2e3703af75e5
swarajpande4 <swarajpande4@gmail.com>
2022-01-08 14:16:13 +0530

    Added Phone Directory

---
+++ d5-9/05-Phone-Directory.cpp
@@ -0,0 +1,28 @@
+// Phone Directory: https://practice.geeksforgeeks.org/problems/phone-directory4628/1/
+
+class Solution
+{
+public:
+    vector<vector<string>> displayContacts(int n, string contact[], string s)
+    {
+        set<string> m;
+        vector<vector<string>> ans(s.size());
+
+        for(int i = 0; i < n; i++)
+            m.insert(contact[i]);
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            for(auto it: m)
+            {
+                if(s.substr(0, i + 1) == it.substr(0, i + 1))
+                    ans[i].push_back(it);
+            }
+
+            if(ans[i].size() == 0)
+                ans[i].push_back(to_string(0));
+        }
+
+        return ans;
+    }
+}
