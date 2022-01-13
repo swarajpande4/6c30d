8d7d9189c8c89cb2269d9b513e194904062e37c4
swarajpande4 <swarajpande4@gmail.com>
2022-01-13 11:55:07 +0530

    Fixed Folder Structure, README and added Minimum sum partition

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Repository containing solutions to [Arsh Goyal's](https://www.linkedin.com/in/ar
 
 | Day | Company | Problem Sheet |
 | ------- | ----------- | -------- |
-| `d0-4` | Goldman Sachs | [Link](https://docs.google.com/document/u/1/d/e/2PACX-1vRgrSl5zCl8P92F0qNuJyDF9v8aqfNd1UB9fQWTb-_aohzhPbZ0GOVbXvfnGHgzbWWdkf9gr7ZgM0lj/pub) |
-| `d5-9` | Amazon | [Link](https://docs.google.com/document/d/1KH9GVaUCET-y5SL5sg6DAnon9XwRRW-sPiyJ2p7FRLs/edit#) |
-| `d10-4` | Microsoft | [Link](https://docs.google.com/document/d/1sSyOTeZBVJExf0oytLVGk6Z34h1usFm4QRkr1Wb5ouk/edit) |
+| `d00-04` | Goldman Sachs | [Link](https://docs.google.com/document/u/1/d/e/2PACX-1vRgrSl5zCl8P92F0qNuJyDF9v8aqfNd1UB9fQWTb-_aohzhPbZ0GOVbXvfnGHgzbWWdkf9gr7ZgM0lj/pub) |
+| `d05-09` | Amazon | [Link](https://docs.google.com/document/d/1KH9GVaUCET-y5SL5sg6DAnon9XwRRW-sPiyJ2p7FRLs/edit#) |
+| `d10-14` | Microsoft | [Link](https://docs.google.com/document/d/1sSyOTeZBVJExf0oytLVGk6Z34h1usFm4QRkr1Wb5ouk/edit) |
---
+++ d10-14/01-Minimum-Sum-Partition.cpp
@@ -0,0 +1,51 @@
+// Minimum sum partition:
+
+class Solution
+{
+public:
+public:
+    vector<int> isSubsetPossible(int arr[], int n, int sum)
+    {
+        bool t[n + 1][sum + 1];
+
+        // Initialization
+        for (int i = 0; i < sum + 1; i++)
+            t[0][i] = false;
+        for (int i = 0; i < n + 1; i++)
+            t[i][0] = true;
+
+        // DP
+        for (int i = 1; i < n + 1; i++)
+        {
+            for (int j = 1; j < sum + 1; j++)
+            {
+                if (arr[i - 1] <= j)
+                    t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j];
+                else
+                    t[i][j] = t[i - 1][j];
+            }
+        }
+
+        // Storing last row in vector
+        vector<int> v;
+        for (int j = 0; j < (sum / 2) + 1; j++)
+            if (t[n][j])
+                v.push_back(j);
+
+        return v;
+    }
+
+    int minDifference(int arr[], int n)
+    {
+        // Your code goes here
+        int range = accumulate(arr, arr + n, 0);
+
+        vector<int> v = isSubsetPossible(arr, n, range);
+
+        int mn = INT_MAX;
+        for (int i = 0; i < v.size(); i++)
+            mn = min(mn, range - 2 * v[i]);
+
+        return mn;
+    }
+}
